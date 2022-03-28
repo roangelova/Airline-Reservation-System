e49aafe9e854298da124fee25a8a11d696ef9e73
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-03-28 19:56:24 +0300

    Admin area added

---
+++ AirlineReservationSystem/AirlineReservationSystem/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using AirlineReservationSystem.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirlineReservationSystem.Areas.Admin.Controllers
+{
+    [Authorize(Roles = UserConstants.Role.AdministratorRole)]
+    [Area("Admin")]
+    public class BaseController : Controller
+    {
+        
+
+
+    }
+}

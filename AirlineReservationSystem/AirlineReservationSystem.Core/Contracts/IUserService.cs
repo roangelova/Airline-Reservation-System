59158ba3f9b950eb809db648440e6478363b365a
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-03-28 19:55:20 +0300

    user service and user constants class added

---
+++ AirlineReservationSystem/AirlineReservationSystem.Core/Contracts/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using AirlineReservationSystem.Core.Models.Users;
+using AirlineReservationSystem.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineReservationSystem.Core.Contracts
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserListVM>> GetUsers();
+    }
+}
---
+++ AirlineReservationSystem/AirlineReservationSystem.Core/UserConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineReservationSystem.Core
+{
+    public static class UserConstants
+    {
+        public static class Role
+        {
+            public const string AdministratorRole = "Administrator";
+        }
+    }
+}

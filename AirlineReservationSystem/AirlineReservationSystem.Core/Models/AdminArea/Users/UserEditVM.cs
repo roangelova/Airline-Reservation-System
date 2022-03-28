3ea73c06e3c599e58875d895fdf568864ab4ec4e
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-03-28 21:06:56 +0300

    useredit and userlist view model added

---
+++ AirlineReservationSystem/AirlineReservationSystem.Core/Models/AdminArea/Users/UserEditVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineReservationSystem.Core.Models.Users
+{
+    public class UserEditVM
+    {
+        public string? LastName { get; set; }
+
+        public string? FirstName { get; set; }
+
+    }
+}
---
+++ AirlineReservationSystem/AirlineReservationSystem.Core/Models/AdminArea/Users/UserListVM.cs
@@ -0,0 +1,10 @@
+﻿namespace AirlineReservationSystem.Core.Models.Users
+{
+    public class UserListVM
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}

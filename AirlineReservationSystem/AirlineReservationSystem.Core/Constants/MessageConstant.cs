3e9d02de3cebda6ee4d67c52a7819e10430bec5f
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-03-30 16:30:27 +0300

    messageconstants added

---
+++ AirlineReservationSystem/AirlineReservationSystem.Core/Constants/MessageConstant.cs
@@ -0,0 +1,10 @@
+﻿namespace AirlineReservationSystem.Core.Constants
+{ 
+        public static class MessageConstant
+        {
+            public const string ErrorMessage = "ErrorMessage";
+            public const string SuccessMessage = "SuccessMessage";
+            public const string WarningMessage = "WarningMessage";
+        }
+    
+}

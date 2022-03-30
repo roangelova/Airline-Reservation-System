e6af68a598b7771f878ad28fa8d97c5fe84bf5cd
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-03-30 16:30:47 +0300

    admin area userController added

---
+++ AirlineReservationSystem/AirlineReservationSystem/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,121 @@
+﻿using AirlineReservationSystem.Core;
+using AirlineReservationSystem.Core.Constants;
+using AirlineReservationSystem.Core.Contracts;
+using AirlineReservationSystem.Core.Models.AdminArea.Users;
+using AirlineReservationSystem.Core.Models.Users;
+using AirlineReservationSystem.Infrastructure.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirlineReservationSystem.Areas.Admin.Controllers
+{
+    public class UserController : BaseController
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        private readonly UserManager<ApplicationUser> userManager;
+
+        private readonly IUserService userService;
+
+        public UserController(
+            RoleManager<IdentityRole> _roleManager,
+            UserManager<ApplicationUser> _userManager,
+            IUserService _userService)
+        {
+            roleManager = _roleManager;
+            userManager = _userManager;
+            userService = _userService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public async Task<IActionResult> ManageUsers()
+        {
+
+            var users = await userService.GetUsers();
+
+            return View(users);
+
+        }
+
+        public async Task<IActionResult> Roles(string id)
+        {
+            var user = await userService.GetUserById(id);
+
+            var model = new UserRolesVM()
+            {
+                UserId = user.Id,
+                Name = $"{user.FirstName} {user.LastName}"
+            };
+
+            ViewBag.RoleItems = roleManager.Roles
+                  .ToList()
+                  .Select(r => new SelectListItem()
+                  {
+                      Text = r.Name,
+                      Value = r.Name,
+                      Selected = userManager.IsInRoleAsync(user, r.Name).Result
+
+                  }).ToList();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Roles(UserRolesVM model)
+        {
+            var user = await userService.GetUserById(model.UserId);
+            var userRoles = await userManager.GetRolesAsync(user);
+            await userManager.RemoveFromRolesAsync(user, userRoles);
+
+            if (model.RoleNames?.Length > 0)
+            {
+                await userManager.AddToRolesAsync(user, model.RoleNames);
+            }
+
+            return RedirectToAction(nameof(ManageUsers));
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            var model = await userService.GetUserForEdit(id);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UserEditVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (await userService.UpdateUser(model))
+            {
+                ViewData[MessageConstant.SuccessMessage] = "Update was successful";
+            }
+            else
+            {
+                ViewData[MessageConstant.SuccessMessage] = "Something went wrong";
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> CreateRole()
+        {
+            //await roleManager.CreateAsync(new IdentityRole()
+            //{
+            //    Name = UserConstants.Role.AdministratorRole
+            //});
+
+            return Ok();
+        }
+    }
+}

9e2433da8e13d86cb3d0813bd23a4bbe9543a002
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-03-30 16:31:58 +0300

    UserEdit and UserRoles ViewModels added

--- AirlineReservationSystem/AirlineReservationSystem.Core/Models/AdminArea/Users/UserEditVM.cs
+++ AirlineReservationSystem/AirlineReservationSystem.Core/Models/AdminArea/Users/UserEditVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,15 @@ namespace AirlineReservationSystem.Core.Models.Users
 {
     public class UserEditVM
     {
-        public string? LastName { get; set; }
+        public string Id { get; set; }
 
-        public string? FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
 
     }
 }
---
+++ AirlineReservationSystem/AirlineReservationSystem.Core/Models/AdminArea/Users/UserRolesVM.cs
@@ -0,0 +1,13 @@
+﻿namespace AirlineReservationSystem.Core.Models.AdminArea.Users
+{
+    public class UserRolesVM
+    {
+        public string UserId { get; set; }
+
+        public string Name { get; set; }
+
+        //all selected roles
+        public string[] RoleNames { get; set; }
+
+    }
+}

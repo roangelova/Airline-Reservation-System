768babb2edb38217fd205126c7dc3485a6990089
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-04-05 21:52:56 +0300

    get, update and delete user added

--- AirlineReservationSystem/AirlineReservationSystem.Core/Contracts/IUserService.cs
+++ AirlineReservationSystem/AirlineReservationSystem.Core/Contracts/IUserService.cs
@@ -5,11 +5,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace AirlineReservationSystem.Core.Contracts
 {
     public interface IUserService
     {
         Task<IEnumerable<UserListVM>> GetUsers();
+
+        Task<UserEditVM> GetUserForEdit(string id);
+
+        Task<bool> UpdateUser(UserEditVM model);
+
+        Task<ApplicationUser> GetUserById(string id);
     }
 }

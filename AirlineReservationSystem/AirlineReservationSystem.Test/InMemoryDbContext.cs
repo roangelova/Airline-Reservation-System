b8ebcd96b5a2916ddc65b3818ab2f403823d5e1f
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-04-05 21:51:45 +0300

    added InMemoryDbContext

---
+++ AirlineReservationSystem/AirlineReservationSystem.Test/InMemoryDbContext.cs
@@ -0,0 +1,37 @@
+﻿using AirlineReservationSystem.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineReservationSystem.Test
+{
+    public class InMemoryDbContext
+
+    {
+        private readonly SqliteConnection connection;
+        private readonly DbContextOptions<ApplicationDbContext> dbContextOptions;
+
+        public InMemoryDbContext()
+        {
+            connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+
+            dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            using var context = new ApplicationDbContext(dbContextOptions);
+
+            context.Database.EnsureCreated();
+        }
+
+        public ApplicationDbContext CreateContext() => new ApplicationDbContext(dbContextOptions);
+
+        public void Dispose() => connection.Dispose();
+
+    }
+}

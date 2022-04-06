38fdfa3a83bd58d720ffcc37c63941725ec41663
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-04-06 17:31:51 +0300

    ServiceCollectionExtension added

---
+++ AirlineReservationSystem/AirlineReservationSystem/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,32 @@
+﻿using AirlineReservationSystem.Core.Contracts;
+using AirlineReservationSystem.Core.Services;
+using AirlineReservationSystem.Data;
+using AirlineReservationSystem.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirlineReservationSystem.Extensions
+{
+    namespace Microsoft.Extensions.DependencyInjection
+    {
+        public static class ServiceCollectionExtension
+        {
+            public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+            {
+                services.AddScoped<IApplicatioDbRepository, ApplicatioDbRepository>();
+                services.AddScoped<IUserService, UserService>();
+
+                return services;
+            }
+
+            public static IServiceCollection AddApplicationDbContexts(this IServiceCollection services, IConfiguration config)
+            {
+                var connectionString = config.GetConnectionString("DefaultConnection");
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(connectionString));
+                services.AddDatabaseDeveloperPageExceptionFilter();
+
+                return services;
+            }
+        }
+    }
+}

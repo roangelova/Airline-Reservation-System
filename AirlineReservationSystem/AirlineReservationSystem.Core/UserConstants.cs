d5eae679357e30f08509eb7474d85cef9bd673a2
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-04-06 18:00:01 +0300

    IdentityUser replaced with ApplicationUser

--- AirlineReservationSystem/AirlineReservationSystem.Core/UserConstants.cs
+++ AirlineReservationSystem/AirlineReservationSystem.Core/UserConstants.cs
@@ -11,6 +11,10 @@ namespace AirlineReservationSystem.Core
         public static class Role
         {
             public const string AdministratorRole = "Administrator";
+
+            public const string FlightManagerRole = "Flight Manager";
+
+            public const string FleetManagerRole = "Fleet Manager";
         }
     }
 }

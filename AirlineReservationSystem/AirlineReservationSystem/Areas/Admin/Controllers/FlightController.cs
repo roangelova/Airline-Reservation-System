67e26cbfcf067bdac76b65e2ef27cd35726c80e0
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-04-08 21:20:41 +0300

    rest of admin controllers added

---
+++ AirlineReservationSystem/AirlineReservationSystem/Areas/Admin/Controllers/FlightController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AirlineReservationSystem.Areas.Admin.Controllers
+{
+    public class FlightController : BaseController
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
---
+++ AirlineReservationSystem/AirlineReservationSystem/Areas/Admin/Controllers/RouteController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AirlineReservationSystem.Areas.Admin.Controllers
+{
+    public class RouteController : BaseController
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddRoute()
+        {
+            return View();
+        }
+
+    }
+}

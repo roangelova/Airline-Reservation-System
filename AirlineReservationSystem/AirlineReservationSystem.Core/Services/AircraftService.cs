c4ceed2e9e4bed675a04c88301b1370b4d2ed969
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-04-08 13:22:03 +0300

    services moved to services folder

---
+++ AirlineReservationSystem/AirlineReservationSystem.Core/Services/AircraftService.cs
@@ -0,0 +1,25 @@
+﻿using AirlineReservationSystem.Core.Contracts;
+using AirlineReservationSystem.Core.Models.AdminArea.Aircraft;
+using AirlineReservationSystem.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineReservationSystem.Core.Services
+{
+    public class AircraftService : IAircraftService
+    {
+        private readonly IApplicatioDbRepository repo;
+
+        public AircraftService(IApplicatioDbRepository _repo)
+        {
+            repo = _repo;
+        }
+        public Task<AddAircraftVM> AddAircraft()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

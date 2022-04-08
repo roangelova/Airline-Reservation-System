0653ae8c0a185b03785eaa475ff62051a952cf01
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-04-08 21:22:29 +0300

    created AddRouteVM

---
+++ AirlineReservationSystem/AirlineReservationSystem.Core/Models/AdminArea/Route/AddRouteVM.cs
@@ -0,0 +1,24 @@
+﻿using AirlineReservationSystem.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineReservationSystem.Core.Models.AdminArea.Route
+{
+    using static DataConstants;
+    public class AddRouteVM
+    {
+
+        [Required]
+        [Display(Name = "City Name")]
+        public string City { get; set; }
+
+        [Required]
+        [StringLength(IATACodeMaxLength)]
+        public string IATA { get; set; }
+
+    }
+}

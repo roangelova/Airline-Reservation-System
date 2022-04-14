54a357d4c249fb7a3b204adcbbf19757ae72f81a
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-04-14 21:13:40 +0300

    updated FlightController

--- AirlineReservationSystem/AirlineReservationSystem/Areas/Admin/Controllers/FlightController.cs
+++ AirlineReservationSystem/AirlineReservationSystem/Areas/Admin/Controllers/FlightController.cs
@@ -1,12 +1,76 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AirlineReservationSystem.Core.Contracts;
+using AirlineReservationSystem.Core.Models.AdminArea.Flight;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AirlineReservationSystem.Areas.Admin.Controllers
 {
     public class FlightController : BaseController
     {
+
+        private readonly IAircraftService AircraftService;
+
+        private readonly IFlightRouteService FlightRouteService;
+        private readonly IFlightService flightService;
+
+        public FlightController(
+            IAircraftService _AircraftService,
+            IFlightRouteService _FlightRouteService,
+            IFlightService _flightService)
+        {
+            AircraftService = _AircraftService;
+            FlightRouteService = _FlightRouteService;
+            flightService = _flightService;
+        }
+
         public IActionResult Home()
         {
             return View();
+        }
+
+        public async Task<IActionResult> AddFlight()
+        {
+            //TODO 
+            //1. all airports for departure
+            //2. airports - departure airport
+            //3.all aircraft
+
+           var availableAircrfats = await AircraftService.GetAllAircraft();
+           var availableDepartures = await FlightRouteService.GetAllDepartureRoutes();
+           
+           
+           
+           ViewBag.AvailableAircraft = availableAircrfats
+               .Select(a => new SelectListItem()
+               {
+                   Text = $"{a.Manufacturer} {a.AircraftModel}",
+                   Value = a.Id
+               })
+               .ToList();
+           
+           ViewBag.AvailableDepartures = availableDepartures
+               .Select(d => new SelectListItem()
+               {
+                   Text = d.City,
+                   Value = d.Id
+               })
+               .ToList();
+
+           
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddFlight(AddFlightVM model)
+        {
+            await flightService.AddFlight(model);
+
+            //TODO: test once url images is added to aircraft model
+            return RedirectToAction("Home");
+
         }
     }
 }

620b039ce762bcf2b75df25fcc363e7b2ea6d044
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-04-14 21:17:40 +0300

    created FlightRouteController

---
+++ AirlineReservationSystem/AirlineReservationSystem/Areas/Admin/Controllers/FlightRouteController.cs
@@ -0,0 +1,40 @@
+﻿using AirlineReservationSystem.Core.Contracts;
+using AirlineReservationSystem.Core.Models.AdminArea.Route;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AirlineReservationSystem.Areas.Admin.Controllers
+{
+    public class FlightRouteController : BaseController
+    {
+        private readonly IFlightRouteService service;
+
+        public FlightRouteController(IFlightRouteService _service)
+        {
+            service = _service;
+        }
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddRoute()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRoute(AddFlightRouteVM flightRouteVM)
+        {
+            await service.AddFlightRoute(flightRouteVM);
+
+            //TODO: REDIRECT TO WHERE??
+
+            return RedirectToAction("Home");
+
+        }
+
+
+    }
+}

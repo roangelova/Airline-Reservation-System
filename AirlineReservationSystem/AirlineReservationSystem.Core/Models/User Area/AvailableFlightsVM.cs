28f7a66efd3f778d6db9376b25596712027c4628
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-04-16 11:50:04 +0300

    created AvailableFlights View Model

---
+++ AirlineReservationSystem/AirlineReservationSystem.Core/Models/User Area/AvailableFlightsVM.cs
@@ -0,0 +1,12 @@
+﻿namespace AirlineReservationSystem.Core.Models.User_Area
+{
+    public class AvailableFlightsVM
+    {
+        public string DepartureDestination { get; set; }
+        public string ArrivalDestination { get; set; }
+
+        public string Price { get; set; }
+
+        public string DateAndTime { get; set; }
+    }
+}

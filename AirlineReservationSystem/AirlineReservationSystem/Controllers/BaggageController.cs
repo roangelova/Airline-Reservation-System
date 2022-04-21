b69e6ecf403846de7f2eb99d8b09e9e5af47d8ce
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-04-21 17:26:14 +0300

    created BaggageController

---
+++ AirlineReservationSystem/AirlineReservationSystem/Controllers/BaggageController.cs
@@ -0,0 +1,30 @@
+﻿using AirlineReservationSystem.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AirlineReservationSystem.Controllers
+{
+    public class BaggageController : BaseController
+    {
+        private readonly IBaggageService baggageService;
+
+        public BaggageController(IBaggageService _baggageService)
+        {
+            baggageService = _baggageService;
+        }
+        public IActionResult AddBaggage()
+        {
+            var availableSizes = baggageService.GetAvailableBaggageSizes();
+
+            ViewBag.AvailableSizes = availableSizes
+                .Select(s => new SelectListItem()
+                {
+                    Text = s.Size,
+                    Value = s.Size
+                })
+                .ToList();
+
+            return View();
+        }
+    }
+}
